391d36c5ebb0b6716598a1154dd8f5d8189adec3
seonghyeon cho <ghssh1@tistory.com>
2016-05-08 14:54:19 +0900

    graph 사용 수정

--- SmGraph/main.cpp
+++ SmGraph/main.cpp
@@ -9,21 +9,34 @@ int main()
 {
 	string filename = "dataset/facebook_combined.txt";
 
-	debug::memory_bomb mb;
+	//memory leak 체크 코드
+	/*debug::memory_bomb mb;
 	{
 		Graph* graph = readGraph(filename);
 		Graph* sm_graph = readSummarizedGraph(filename);
 		delete graph;
 		delete sm_graph;
 	}
-	mb.leakCheck();
-	int s, t;
+	mb.leakCheck();*/
+
+	//prev test code
+	/*int s, t;
 	for (int i = 0; i < 1000; i++)
 	{
 		s = random::Int(0, 4000);
 		t = random::Int(0, 4000);
-		//DynamicSummarization::addEdge(sm_graph, graph, 0, 100);
-	}
+		DynamicSummarization::addEdge(sm_graph, graph, s, t);
+	}*/
+
+
+	Graph* graph = readGraph(filename);
+	Graph* sm_graph = readSummarizedGraph(filename);
+
+	//unit test -> edge 1개 추가 후 테스트
+	DynamicSummarization::addEdgeAndSummarize(*sm_graph, *graph, 0, 1);
+
+	delete graph;
+	delete sm_graph;
 
 	return 0;
 }
