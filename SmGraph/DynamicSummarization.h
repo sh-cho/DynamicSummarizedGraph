c7b20763c85cdd757091d6b19b8d8973e1774351
seonghyeon cho <ghssh1@tistory.com>
2016-05-08 16:41:55 +0900

    반복문 안에서 돌아가도록 변경

--- SmGraph/DynamicSummarization.h
+++ SmGraph/DynamicSummarization.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Graph.h"
 #include "SummarizedStructure.hpp"
+#include "random.hpp"
 
 class DynamicSummarization
 {
@@ -30,75 +31,141 @@ public:
 		}
 		return neighbors;
 	}
+
 	static int getParent(Node* p)
 	{
 		NormalNode* np = (NormalNode*)p;
 		return np->getParent();
 	}
+
 	static bool sameParentAndId(NormalNode* np1)
 	{
 		return np1->getParent() == np1->getId();
 	}
-	static double cham_go_yong(Graph& sg, Graph& origin, int id1, int id2)
-	{
-		//참고용
-		auto& ns1 = getNeighborNodes(sg, origin, id1);
-		auto& ns2 = getNeighborNodes(sg, origin, id2);
-		int total = ns1.size() + ns2.size();
-		set<int> set1(ns1.begin(), ns1.end());
-		set<int> set2(ns2.begin(), ns2.end());
-		vector<int> result;
-		set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(result));
-		double case1_cost = (double)(result.size() + set1.size() - result.size() + set2.size() - result.size()) / total;
-		static double advance_count = 0;
-		advance_count += (1 - case1_cost) * total;
-		cout << "====>advance_count:" << advance_count << endl;
-		//모두 +로 묵는경우
-		if (1 - case1_cost > 0)
-		{
-			auto* p1 = sg.get(id1);
-			auto* p2 = sg.get(id2);
-			auto& p1_items = p1->getEdges();
-			auto& p2_items = p2->getEdges();
-			NormalNode* np1 = (NormalNode*)p1;
-			NormalNode* np2 = (NormalNode*)p2;
-			if (!sameParentAndId(np1) && !sameParentAndId(np2))
-			{
-				result.clear();
-				set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(result));
-				for (int id : result) {
-					sg.add(new Edge(id2, id));
-				}
-				result.clear();
-				set_difference(set2.begin(), set2.end(), set1.begin(), set1.end(), std::back_inserter(result));
-				for (int id : result) {
-					sg.add(new Edge(id1, id));
-				}
-			}
-			else if (!sameParentAndId(np1))
-			{
-				result.clear();
-				set_difference(set2.begin(), set2.end(), set1.begin(), set1.end(), std::back_inserter(result));
-				for (int id : result) {
-					sg.add(new Edge(id2, id));
-				}
-			}
-			else if (!sameParentAndId(np2))
-			{
-				result.clear();
-				set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(result));
-				for (int id : result) {
-					sg.add(new Edge(id1, id));
-				}
-			}
-		}
-		return 1;
-	}
+
 	static void dynamicSummarize(Graph& sg, Graph& origin, int id1, int id2)
 	{
 		//cost 계산 뒤 case 나눠서 요약
-	}
-	static void addEdge(Graph& sg, Graph& origin, int src, int trg)
+		auto& ns1 = getNeighborNodes(sg, origin, id1);
+		auto& ns2 = getNeighborNodes(sg, origin, id2);
+		set<int> set1(ns1.begin(), ns1.end());
+		set<int> set2(ns2.begin(), ns2.end());
+		
+		set<int> subgraph;
+		set<int> checkedNodeList;
+
+
+		//add subraph to A, A_2, B, B_2
+		subgraph.emplace(id1);
+		for (int nodeID : ns1)
+			subgraph.emplace(nodeID);
+		subgraph.emplace(id2);
+		for (int nodeID : ns2)
+			subgraph.emplace(nodeID);
+
+
+
+		vector<int> result;
+		//전부 check될 때까지 반복
+		while (subgraph.size() > checkedNodeList.size())
+		{
+			//get u, v
+			int u, v;
+			auto enditer = checkedNodeList.end();
+			
+			bool noUExist = false,
+				noU2Exist = false;
+			
+
+			do {
+				u = getRandomNodeInSubgraph(sg, origin, subgraph);	//any unchecked node in subgraph
+				v = getRandomNodeInSubgraph(sg, origin, subgraph, u);	//any unchecked U2 in subraph
+
+
+				if (u == -1)
+				{
+					noUExist = false;
+					break;
+				}
+
+				if (v == -1)	//v is not exist
+				{
+					noU2Exist = true;
+					break;
+				}
+			} while (checkedNodeList.find(u)!=enditer
+				&& checkedNodeList.find(v)!=enditer);
+
+			if (noUExist)	//u가 없을 경우
+			{
+				continue;
+			}
+			else if (noU2Exist)	//u에 해당하는 u2가 없을 경우 -> check u
+			{
+				checkedNodeList.emplace(u);
+				continue;
+			}
+
+			
+			result.clear();
+			//summarize check
+			if (getSummarizeRatio(sg, origin, u, v) > 0)
+			{
+				//do summarize
+
+				auto* sNode = sg.get(u);
+				auto* tNode = sg.get(v);
+				auto& sEdges = sNode->getEdges();
+				auto& tEdges = tNode->getEdges();
+
+				NormalNode* srcNode = (NormalNode*)sNode;
+				NormalNode* trgNode = (NormalNode*)tNode;
+
+				//1. in same supernode
+				//2. in diff supernode
+				//3. supernode - normalnode
+				//4. normalnode
+				if (!sameParentAndId(srcNode) && !sameParentAndId(trgNode))	//1, 2
+				{
+					std::set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(result));
+					for (int nodeID : result)
+						sg.add(new Edge(id2, nodeID));
+
+					result.clear();
+					std::set_difference(set2.begin(), set2.end(), set1.begin(), set1.end(), std::back_inserter(result));
+					for (int nodeID : result)
+						sg.add(new Edge(id1, nodeID));
+
+
+					//w, w2 추가
+				}
+				else if (!sameParentAndId(srcNode)) //3-1. src is in SuperNode
+				{
+					result.clear();
+					std::set_difference(set2.begin(), set2.end(), set1.begin(), set1.end(), std::back_inserter(result));
+					for (int nodeID : result)
+						sg.add(new Edge(id2, nodeID));
+				}
+				else if (!sameParentAndId(trgNode)) //3-2. trg is in SuperNode
+				{
+					std::set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(result));
+					for (int nodeID : result)
+						sg.add(new Edge(id1, nodeID));
+				}
+				else //4
+				{
+					//
+
+				}
+			}
+			else
+			{
+				checkedNodeList.emplace(v);
+			}
+		} //while
+	} //dynamicSummarize
+
+	static void addEdgeAndSummarize(Graph& sg, Graph& origin, int src, int trg)
 	{
 		//    auto* snode = sg.get(src);
 		//    auto* tnode = sg.get(trg);
@@ -129,6 +196,60 @@ public:
 
 		//2. do summarize
 		dynamicSummarize(sg, origin, src, trg);
+	}
+
+	static double getSummarizeRatio(Graph& sg, Graph& origin, int id1, int id2)
+	{
+		//return s(id1, id2)
+
+		auto& ns1 = getNeighborNodes(sg, origin, id1);
+		auto& ns2 = getNeighborNodes(sg, origin, id2);
+		int total = (int)ns1.size() + (int)ns2.size();
+		set<int> set1(ns1.begin(), ns1.end());
+		set<int> set2(ns2.begin(), ns2.end());
+		vector<int> interSection;
+		set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(interSection));
+
+		cout << "ratio:" << (double)(set1.size() + set2.size() - interSection.size()) / (total) << endl;
+
+		return (double)(set1.size() + set2.size() - interSection.size())/(total);
+	}
+
+	static int getRandomNodeInSubgraph(Graph& sg, Graph& origin, set<int> subgraph, int u = -1)
+	{
+		//if u == -1 (no input u)
+		//  get random u
+		//else
+		//  get u's 2-hop node(u2) in subgraph
+
+
+		int nodeID = -1;	//-1: Not found
+		set<int>::iterator it = subgraph.begin();
+
+		if (u == -1)	//get random u node
+		{
+			advance(it, random::Int(0, (int)subgraph.size()-1));
+
+			if (it != subgraph.end())
+				nodeID = *it;
+			//else //not found
+			//	nodeID = -1;
+		}
+		else	//get u2 node
+		{
+			auto& neighborNodes = getNeighborNodes(sg, origin, u);
+			for (int neighborID : neighborNodes)
+			{
+				if (subgraph.find(neighborID) != subgraph.end())
+				{
+					nodeID = neighborID;
+					break;
+				}
+			}
+		}
+
+
+		return nodeID;
 	}
 };
 
