2af07326984a38ffb263f46174c552910cde3c6c
seonghyeon cho <ghssh1@tistory.com>
2016-05-15 22:25:28 +0900

    randomized 방식 변경

--- Randomize/main.cpp
+++ Randomize/main.cpp
@@ -1,11 +1,17 @@
 /**
  *	Graph 파일 읽어서 70%의 Edge만 남겨서 새로운 Graph 파일로 저장
  *	30%의 edge는 따로 파일로 저장
+ *
+ *	INPUT_NAME에 .txt 확장자 제외한 이름 입력
+ *	파일에 주석 등이 없어야함
+ *	ex) #define INPUT_NAME "dataset/facebook_combined"
+ *
+ *	ERASE_RATE는 지울 비율 (0 ~ 1)
+ *	0.01 단위까지만 지원함
  */
 
-#define	INPUT_NAME	"dataset/facebook_combined"
-//#define	RANDOMIZED_FILE	"facebook_randomized.txt"
-//#define EDGES_FILE	"facebook_randomized_edges.txt"
+#define	INPUT_NAME	"dataset/com-dblp.ungraph"	
+#define	ERASE_RATE	0.1
 
 #include <ctime>
 #include <iostream>
@@ -20,8 +26,8 @@ int main()
 	srand((unsigned)time(nullptr));
 
 	string inputFileName = INPUT_NAME + string(".txt");
-	string randomizedFileName = inputFileName.substr(0, inputFileName.find('.')) + "_randomized.txt";	//->randomly delete
-	string edgesFileName = inputFileName.substr(0, inputFileName.find('.')) + "_edges.txt";
+	string randomizedFileName = inputFileName.substr(0, inputFileName.find_last_of('.')) + "_randomized.txt";	//->randomly delete
+	string edgesFileName = inputFileName.substr(0, inputFileName.find_last_of('.')) + "_edges.txt";
 
 	
 	ifstream fin(inputFileName);
@@ -34,6 +40,7 @@ int main()
 	}
 
 
+	cout << "* Start randomly delete " << ERASE_RATE*100 << "% edge" << endl;
 	
 	//모든 Edge 읽어서 pair<int, int> vector에 저장
 	//70% 남을 때까지 중간에 빼서 erase 후 EDGES_FILE에 저장
@@ -42,16 +49,30 @@ int main()
 	vector<Edge> outEdgeList;
 
 	int from, to;
-	int edgeCnt = 0;
+	int wholeEdgeCnt = 0, delCnt = 0;
 	while (fin >> from >> to)
 	{
-		inEdgeList.push_back(Edge({ from, to }));
-		++edgeCnt;
+		double val = rand() % 100;
+		if (val < ERASE_RATE * 100)
+		{
+			outEdgeList.push_back(Edge({ from, to }));
+			++delCnt;
+		}
+		else
+		{
+			inEdgeList.push_back(Edge({ from, to }));
+		}
+
+		++wholeEdgeCnt;
 	}
+	cout << "* wholeEdgeCnt: " << wholeEdgeCnt << endl;
+	cout << "* randomized Edge Count: " << wholeEdgeCnt - delCnt << endl;
+	cout << "* deleteCnt: " << delCnt << endl;
 
-
-	int delEdgeNum = (int)(edgeCnt * 0.3);
+	/*int delEdgeNum = (int)(edgeCnt * 0.3);
 	int randIdx;
+
+	cout << "* delEdgeNum: " << delEdgeNum << endl;
 	while (delEdgeNum > 0)
 	{
 		randIdx = rand() % inEdgeList.size();
@@ -60,20 +81,20 @@ int main()
 		inEdgeList.erase(inEdgeList.begin() + randIdx);
 
 		--delEdgeNum;
-	}
+	}*/
 
 
-	//randomized file 출력
-	for (auto edge : inEdgeList)
+	cout << "* file output start" << endl;
+	for (auto edge : inEdgeList)	//randomized file 출력
 	{
 		frand << edge.first << "\t" << edge.second << endl;
 	}
 
-	//edge file 출력
-	for (auto edge : outEdgeList)
+	for (auto edge : outEdgeList)	//edge file 출력
 	{
 		fedge << edge.first << "\t" << edge.second << endl;
 	}
+	cout << "* file output end" << endl;
 
 
 	fin.close();
